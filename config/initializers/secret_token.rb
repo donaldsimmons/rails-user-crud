9ff9d604bcc95fe98fad0862c26217741917bf5f
Trey Simmons <yert511@gmail.com>
2013-09-30 19:40:25 -0500

    Added base file structure for the application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserCrud::Application.config.secret_key_base = '134a0aa74d34e4bc59e8e5d6290abfe492db3c8a9edccc5892672a3fc53a2380dc7b32cde4882f43fa53bf979bd84680502780493c16a0867569221a5b12de89'
